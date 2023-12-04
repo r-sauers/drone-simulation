d8ddc8377bd99fe29bfb709ffc8cb28ade636973
Evan Burkstrand <burks079@umn.edu>
2023-12-03 22:40:07 -0600

    Started DataCollection class

---
+++ libs/transit/include/DataCollection.h
@@ -0,0 +1,14 @@
+#ifndef DATA_COLLECTION_H_
+#define DATA_COLLECTION_H_
+
+/**
+ *@brief 
+*/
+
+class DataCollection {
+    public:
+        //some functions that track nodes
+    private:
+        int rightTurns = 0;
+        int leftTurns = 0;
+}
