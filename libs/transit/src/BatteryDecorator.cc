0a5a0a5ff635ded4b874835cb82e49136114e50e
Jacob Lopez <lope0384@umn.edu>
2023-12-14 16:49:15 -0600

    Drone Battery drains over time implemented

--- libs/transit/src/BatteryDecorator.cc
+++ libs/transit/src/BatteryDecorator.cc
@@ -7,7 +7,7 @@ BatteryDecorator::BatteryDecorator(Drone* drone, double batteryPower)
   this->batteryPower = batteryPower;
   this->charging = 0;
   this->drone = drone;
-  // this->station = chargeStations.back();
+  // this->station = this->model->chargeStations.back();
 }
 
 double BatteryDecorator::getBatteryPower() { return batteryPower; }
@@ -21,28 +21,29 @@ void BatteryDecorator::update(double dt) {
     this->drone->linkModel(model);
     this->linked = 1;
   }
-  this->drone->update(dt);
-  this->setPosition(this->drone->getPosition());
-  // if (this->charging) {
-  //   this->batteryPower += dt * 20;  // Charges over 5 seconds
-  //   if (this->batteryPower >= 100) {
-  //     this->charging = 0;
-  //     this->batteryPower = 100;
-  //     this->station->update(this->drone->getID());  // Undock
-  //   }
-  // } else {
-  //   this->batteryPower -= dt * 2;  // Drains over 50 seconds
-  //   if (this->batteryPower > 0) {
-  //     if ((this->drone->getPosition() == this->station->getPosition()) &&
-  //         (this->drone->getSeekingCharge() == 1)) {
-  //       if (this->station->update(this->drone->getID())) {  // Try to dock
-  //         this->charging = 1;
-  //         this->drone->setSeekingCharge(0);
-  //       } else {
-  //         this->drone->update(dt);
-  //       }
-  //     }
-  //   }
-  // }
+
+  std::cout << "Battery Power: " << this->batteryPower << std::endl;
+  if (this->charging) {
+    this->batteryPower += dt * 20;  // Charges over 5 seconds
+    if (this->batteryPower >= 100) {
+      this->charging = 0;
+      this->batteryPower = 100;
+      // this->station->update(this->drone->getID());  // Undock
+    }
+  } else {
+    if (this->batteryPower > 0) {
+      this->batteryPower -= dt * 2;  // Drains over 50 seconds
+      // if ((this->drone->getPosition() == this->station->getPosition()) &&
+      //     (this->drone->getSeekingCharge() == 1)) {
+      //   if (this->station->dockDrone(this->drone->getID())) {  // Try to dock
+          // this->charging = 1;
+          // this->drone->setSeekingCharge(0);
+        // } else {
+      this->drone->update(dt);
+      this->setPosition(this->drone->getPosition());
+        // }
+      // }
+    }
+  }
 }
 
