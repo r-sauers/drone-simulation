560ebba76e8dc463fc47a91415c69cde6bd18aa4
Evan Burkstrand <burks079@umn.edu>
2023-12-16 14:23:39 -0600

    More functionality to Data Collection

--- libs/transit/include/DataCollection.h
+++ libs/transit/include/DataCollection.h
@@ -11,15 +11,26 @@
 
 class DataCollection {
  public:
-    void turnRight();
-    void turnLeft();
-    void resetTurns();
-    std::string getLastStrategy();
-    void getBatteryLevel(double batteryLevel);
-    void getStrategy(std::string strategy);
-    void getDronePath(std::vector<float> start, std::vector<float> end, const routing::RoutingStrategy& pathing, const routing::IGraph* g);
+
+   static DataCollection& getInstance();
+   
+   void turnRight();
+   void turnLeft();
+   void resetTurns();
+   std::string getLastStrategy();
+   void getBatteryLevel(double batteryLevel);
+   void getStrategy(std::string strategy);
+   void getDronePath(std::vector<float> start, std::vector<float> end, const routing::RoutingStrategy& pathing, const routing::IGraph* g);
+   void writeStrategysToCSV();
+   void writeBatteryToCSV();
   // some functions that track nodes
  private:
+
+   DataCollection();
+   ~DataCollection();
+   DataCollection(const DataCollection&) = delete;
+   DataCollection& operator=(const DataCollection&) = delete;
+   
     int rightTurns = 0;
     int leftTurns = 0;
     std::string lastStrategy;
@@ -27,6 +38,10 @@ class DataCollection {
     std::vector< std::vector<float> > posPath;
     std::vector<double> batteryLevel;
     std::vector<std::string> strategys;
+    std::vector<int> rightTurnsVec;
+    std::vector<int> leftTurnsVec;
+    std::string csvFilename = "strategyData.csv";
+    std::string csvFilename2 = "batteryData.csv";
 };
 
 #endif
--- libs/transit/src/DataCollection.cc
+++ libs/transit/src/DataCollection.cc
@@ -1,5 +1,12 @@
 #include "DataCollection.h"
 #include "SimulationModel.h"
+#include <iostream>
+#include <fstream>
+
+DataCollection& DataCollection::getInstance(){
+    static DataCollection instance;
+    return instance;
+}
 
 void DataCollection::turnRight(){
     rightTurns += 1;
@@ -10,11 +17,13 @@ void DataCollection::turnLeft(){
 }
 
 void DataCollection::resetTurns(){
+    rightTurnsVec.push_back(rightTurns);
+    leftTurnsVec.push_back(leftTurns);
     leftTurns = 0;
     rightTurns = 0;
 }
 
- std::string DataCollection::getLastStrategy(){
+std::string DataCollection::getLastStrategy(){
     return lastStrategy;
 }
 
@@ -33,4 +42,35 @@ void DataCollection::getDronePath(std::vector<float> start, std::vector<float> e
     //write/save old path maybe somehow
 
     posPath = positionPath; 
+}
+
+void DataCollection::writeStrategysToCSV(){
+    std::ofstream outputFile(csvFilename);
+
+    if(!outputFile.is_open()){
+        std::cerr << "Error Opening csv file" << std::endl;
+    }
+
+    for (int i = 0; i < strategys.size(); i++) {
+        outputFile << strategys[i] << ": " << rightTurnsVec[i] << ", " << leftTurnsVec[i] << "\n";
+    }
+
+    outputFile.close();
+}
+
+void DataCollection::writeBatteryToCSV(){
+    std::ofstream outputFile(csvFilename2);
+
+    if(!outputFile.is_open()){
+        std::cerr << "Error Opening csv file" << std::endl;
+    }
+
+    for (int i : batteryLevel) {
+        outputFile << batteryLevel[i];
+        if(i < batteryLevel.size() - 1) {
+            outputFile << ", ";
+        }
+    }
+
+    outputFile.close();
 }
